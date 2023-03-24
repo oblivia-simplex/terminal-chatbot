6a0e297e2b57b7ed414f8b0d87e57fcf30f37fae
Lucca Fraser <lucca.fraser@gmail.com>
2023-03-24 12:01:51 -0300

    now strips colour if output redirected

--- ai.py
+++ ai.py
@@ -171,6 +171,9 @@ def truncate(dump):
 
 
 def color_text(text, color):
+    """Only colors the text if the output isn't being redirected or piped."""
+    if not sys.stdout.isatty():
+        return text
     colors = {'red': 31, 'green': 32, 'yellow': 33, 'blue': 34, 'magenta': 35, 'cyan': 36, 'white': 37}
     return f"\033[{colors[color]}m{text}\033[0m"
 
